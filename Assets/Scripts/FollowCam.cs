e28d9ca2941c1867ed495f366af01680cc18eb90
bursalma <bursalma@gmail.com>
2017-10-03 10:57:23 -0400

    Seventh

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+
+    static public FollowCam S;
+    public float easing = 0.05f;
+    public Vector2 minXY;
+    public bool _______________;
+    public GameObject poi;
+    public float camZ;
+
+    void Awake()
+    {
+        S = this;
+        camZ = this.transform.position.z;
+    }
+	
+	void FixedUpdate () {
+        if (poi == null) return;
+        Vector3 destination = poi.transform.position;
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        destination.z = camZ;
+        transform.position = destination;
+        this.GetComponent<Camera>().orthographicSize = destination.y + 10;
+	}
+}
