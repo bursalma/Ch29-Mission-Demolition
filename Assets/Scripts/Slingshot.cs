95fd368eb90ba69cd6c3e465e8f6c27eea195a0e
bursalma <bursalma@gmail.com>
2017-10-02 21:22:33 -0400

    Sixth

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Slingshot : MonoBehaviour {
 
     public GameObject prefapProjectile;
+    public float velocityMult = 4f;
     public bool ___________________;
     public GameObject launchpoint;
     public Vector3 launchPos;
@@ -36,5 +37,30 @@ public class Slingshot : MonoBehaviour {
         projectile = Instantiate(prefapProjectile) as GameObject;
         projectile.transform.position = launchPos;
         projectile.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    private void Update()
+    {
+        if (!aimingMode) return;
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+        if (Input.GetMouseButtonUp(0))
+        {
+            aimingMode = false;
+            projectile.GetComponent<Rigidbody>().isKinematic = false;
+            projectile.GetComponent<Rigidbody>().velocity = -mouseDelta * velocityMult;
+            projectile = null;
+
+        }
     }
 }
